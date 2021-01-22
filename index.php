3603c91c057fd9f1b0b7f30294fb1d21ac0c9b89
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-01-22 14:20:53 +0100

    htgrf

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
 
 // Compléter le code ci dessous pour afficher la longueur de la chaine de caracteres
-
+$google = chr("google");
 echo      ("google");
 
 //echo"<br><br>"; //Decommentez les <br> pour tester l'affichage lorsque vous aurez plusieurs instructions
 
 // Compléter le code ci dessous pour inverser la chaine de caractere
-
+$anti = strrev("anticonstitutionellement");
 echo  ("anticonstitutionellement");
 
 // Et si vous testiez également avec votre prénom ?
 
-
+echo strrev("samuel");
 
 //echo"<br><br>";
 
 // Compléter le code ci dessous pour remplacer le mot ok par non
 $oldtxt = "Ok Google!";
-$newtxt = (, , );
 
-echo $newtxt;
+
+echo str_replace("Ok","sam",$oldtxt);
